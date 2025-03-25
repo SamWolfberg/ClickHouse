50a3a221dcfbd79bea45de8a36097b44e643de95
Sema Checherinda <checherinda@gmail.com>
2025-03-25 17:09:30 +0100

    fix 03221 02884 02765

--- src/Interpreters/ProcessList.cpp
+++ src/Interpreters/ProcessList.cpp
@@ -557,8 +557,6 @@ void QueryStatus::removePipelineExecutor(PipelineExecutor * e)
 
 bool QueryStatus::checkTimeLimit()
 {
-    LOG_DEBUG(getLogger("QueryStatus"), "checkTimeLimit at continuing {}", overflow_mode);
-
     auto elapsed_ns = watch.elapsed();
     throwProperExceptionIfNeeded(limits.max_execution_time.totalMilliseconds(), elapsed_ns);
 
