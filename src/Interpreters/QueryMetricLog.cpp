83db22a60db99bd446ad4798d49ea7d07f54e0b1
Pablo Marcos <pablo.marcos.oltra@clickhouse.com>
2025-03-31 11:45:59 +0000

    Dummy change to test the CI

--- src/Interpreters/QueryMetricLog.cpp
+++ src/Interpreters/QueryMetricLog.cpp
@@ -24,7 +24,7 @@
 namespace DB
 {
 
-static auto logger = getLogger("QueryMetricLog");
+static auto logger = getLogger("QueryMetricLogPablo");
 
 String timePointToString(QueryMetricLog::TimePoint time)
 {
